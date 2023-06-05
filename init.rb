4720a5b7d911def301160650c82104b0a7788cda
Yuya.Nishida <yuya@j96.org>
2023-06-05 22:47:00 +0900

    run: git cococo --init=plugins/redmine_the_never_deleting_story bundle exec rails generate redmine_plugin redmine_the_never_deleting_story

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_the_never_deleting_story do
+  name 'Redmine The Never Deleting Story plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
